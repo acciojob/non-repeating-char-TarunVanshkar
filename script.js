ebd7015a705fe6c10d81100c399e4186b69daf37
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-04-10 16:54:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+function firstNonRepeatedChar(s) {
+  let charArray = new Array(26).fill(0);
+  
+  // To Count the frequency of each character
+  for(let c of s) {
+    let index = c.charCodeAt(0) - 97; // get index of char in the array
+    charArray[index]++;
+  }
+  
+  // Find the first character with frequency 1
+  for(let c of str) {
+    let index = c.charCodeAt(0) - 97; // get index of char in the array
+    if (charArray[index] === 1) {
+      return c;
+    }
+  }
+  
+  // No non-repeated character found
+  return null;
+}
